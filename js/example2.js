4efa95b9d292731ae970b30b71f26ca345148477
lqq <lqq@simplechain.com>
2021-09-09 13:58:11 +0800

    陀螺仪：正方体适配

--- js/example2.js
+++ js/example2.js
@@ -5,11 +5,8 @@ var vConsole = new VConsole();
 var nextRX=0,nextRY=0;
 function init(){
     scene = new THREE.Scene();
-
     camera = new THREE.PerspectiveCamera(45, document.body.clientWidth / document.body.clientHeight, 0.1, 100);
     camera.position.set(0, 0, 3);
-
-
     renderer = new THREE.WebGLRenderer({
         antialias:true,
         alpha:true
@@ -17,9 +14,6 @@ function init(){
     renderer.setSize(document.body.clientWidth, document.body.clientHeight);
     document.getElementById("container").appendChild(renderer.domElement);
 
-    // var controls = new THREE.OrbitControls(camera, renderer.domElement);
-
-    // loadAllTexture(loadCarModel);
 
     const geometry = new THREE.BoxGeometry( .6, .6, .6 );
     const material = new THREE.MeshStandardMaterial( {color: 0x6698CB} );
@@ -30,9 +24,7 @@ function init(){
     scene.add( cube );
 
     addLight();
-
     loop();
-
 
       var video = document.getElementById('video');
       var canvas = document.getElementById('canvas');
@@ -79,23 +71,14 @@ function addLight(){
     bottomRightDirLight.position.y=3;
     bottomRightDirLight.position.z=-5;
     bottomRightDirLight.intensity=1.3;
-
-    // var helper=new THREE.DirectionalLightHelper(bottomRightDirLight,1);
-    // scene.add( helper );
     scene.add( bottomRightDirLight );
 
     var frontDirLight = new THREE.DirectionalLight(0xffffff);
-
     frontDirLight.position.x=-5;
     frontDirLight.position.y=5;
     frontDirLight.position.z=5;
     frontDirLight.intensity=1.3;
-    //directionalLight.castShadow=true;
-
-    // var helper=new THREE.DirectionalLightHelper(frontDirLight,1);
-    // scene.add( helper );
     scene.add( frontDirLight );
-    // 车子正前上方斜45度的灯结束
 }
 
 function loop() {
--- js/example3.js
+++ js/example3.js
@@ -3,11 +3,8 @@ var scene, camera, renderer,cube;
 var vConsole = new VConsole();
 function init(){
     scene = new THREE.Scene();
-
     camera = new THREE.PerspectiveCamera(40, document.body.clientWidth / document.body.clientHeight, 0.1, 100);
     camera.position.set(0, 0, 3);
-
-
     renderer = new THREE.WebGLRenderer({
         antialias:true,
         alpha:true
@@ -15,11 +12,8 @@ function init(){
     renderer.setSize(document.body.clientWidth, document.body.clientHeight);
     document.getElementById("container").appendChild(renderer.domElement);
 
-    // var controls = new THREE.OrbitControls(camera, renderer.domElement);
 
-    // loadAllTexture(loadCarModel);
-
-    const geometry = new THREE.BoxGeometry( 1.2, 1.2, 1.2 );
+    const geometry = new THREE.BoxGeometry( 0.5, 0.5, 0.5 );
     const material = new THREE.MeshStandardMaterial( {color: 0x6698CB} );
     material.metalness = .44;
     material.roughness = 0.4;
@@ -28,11 +22,9 @@ function init(){
     scene.add( cube );
 
     addLight();
-
     loop();
 
-
-  function capture_orientation (event) {
+    function capture_orientation (event) {
       var alpha = event.alpha;
       var beta = event.beta;
       var gamma = event.gamma;
@@ -71,7 +63,7 @@ function addBox(){
 
 function addLight(){
     var ambientLight = new THREE.AmbientLight( 0xd5d5d5 );
-    ambientLight.intensity=1.1;
+    ambientLight.intensity = 1;
     scene.add( ambientLight );
 
     var bottomRightDirLight = new THREE.DirectionalLight();
@@ -79,30 +71,19 @@ function addLight(){
     bottomRightDirLight.position.y=3;
     bottomRightDirLight.position.z=-5;
     bottomRightDirLight.intensity=1.3;
-
-    // var helper=new THREE.DirectionalLightHelper(bottomRightDirLight,1);
-    // scene.add( helper );
     scene.add( bottomRightDirLight );
 
     var frontDirLight = new THREE.DirectionalLight(0xffffff);
-
     frontDirLight.position.x=-5;
     frontDirLight.position.y=5;
     frontDirLight.position.z=5;
     frontDirLight.intensity=1.3;
-    //directionalLight.castShadow=true;
-
-    // var helper=new THREE.DirectionalLightHelper(frontDirLight,1);
-    // scene.add( helper );
     scene.add( frontDirLight );
-    // 车子正前上方斜45度的灯结束
 }
 
 function loop() {
     requestAnimationFrame(loop);
-
     cube.rotation.y+=0.01
-
     renderer.render(scene, camera);
 }
 
